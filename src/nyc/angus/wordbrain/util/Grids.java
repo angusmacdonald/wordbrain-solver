5458a8bf2ff24a7b56b2772bb23e3483031690e1
Angus Macdonald <amacdonald@aetherworks.com>
2015-01-15 14:39:03 -0500

    comment (util): add comments

--- src/nyc/angus/wordbrain/util/Grids.java
+++ src/nyc/angus/wordbrain/util/Grids.java
@@ -1,41 +1,59 @@
 package nyc.angus.wordbrain.util;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Utility for working with 2D grids of characters.
+ */
 public class Grids {
 
-	public static void main(String[] args) {
-		char[][] grid = createGrid();
-		printGrid(grid);
+	/**
+	 * Create a new grid, removing the specified grid elements, and dropping any characters above these elements down,
+	 * so that the only empty (' ') characters are at the top of the grid. The latter operation is termed 'gravity',
+	 * since the remaining letters fall down if there is nothing but space below them.
+	 * 
+	 * @param grid
+	 *        The grid to be changed (a copy is made before alteration, so this isn't changed).
+	 * @param positionsToRemove
+	 *        The grid positions to remove.
+	 * @return A new grid, with the specified grid positions removed, and gravity applied to the remaining elements.
+	 */
+	public static char[][] removeElementsAndApplyGravity(final char[][] grid, final Set<Position> positionsToRemove) {
 
-		Set<Position> positionsSeen = new HashSet<>();
+		final char[][] newGrid = cloneGrid(grid);
 
-		positionsSeen.add(new Position(0, 2));
-		positionsSeen.add(new Position(0, 1));
+		clearSpecifiedPositions(newGrid, positionsToRemove);
 
-		char[][] newGrid = createNewGrid(grid, positionsSeen);
+		applyGravity(newGrid);
 
-		System.out.println("");
-		printGrid(newGrid);
+		return newGrid;
 	}
 
-	public static char[][] createNewGrid(char[][] grid, Set<Position> positionsSeen) {
-
-		char[][] newGrid = cloneArray(grid);
-
-		for (Position position : positionsSeen) {
-			newGrid[position.y][position.x] = ' ';
+	/**
+	 * Clear the specified positions in the grid, replacing the previous character with the ' ' character.
+	 */
+	private static void clearSpecifiedPositions(final char[][] grid, final Set<Position> positionsToRemove) {
+		for (final Position position : positionsToRemove) {
+			grid[position.y][position.x] = ' ';
 		}
+	}
 
-		for (int y = 0; y < grid.length; y++) {
-			for (int x = 0; x < grid[0].length; x++) {
+	/**
+	 * If there is an empty space (a ' ' char) beneath any item in the grid, drop the character down.
+	 * <p>
+	 * This operation leaves space only at the top of the grid.
+	 * <p>
+	 * The grid passed in to this operation is modified.
+	 */
+	private static void applyGravity(final char[][] newGrid) {
+		for (int y = 0; y < newGrid.length; y++) {
+			for (int x = 0; x < newGrid[0].length; x++) {
 
 				if (newGrid[y][x] == ' ') {
 					char prev = ' ';
 					for (int i = 0; i <= y; i++) {
-						char temp = newGrid[i][x];
+						final char temp = newGrid[i][x];
 						newGrid[i][x] = prev;
 						prev = temp;
 					}
@@ -43,43 +61,28 @@ public class Grids {
 
 			}
 		}
-		return newGrid;
 	}
 
-	private static char[][] cloneArray(char[][] original){
-		   return Arrays.stream(original)
-		             .map((char[] row) -> row.clone())
-		             .toArray((int length) -> new char[length][]);
+	/**
+	 * Clone the provided grid.
+	 */
+	private static char[][] cloneGrid(final char[][] original) {
+		return Arrays.stream(original).map((final char[] row) -> row.clone()).toArray((final int length) -> new char[length][]);
 	}
 
-	private static void printGrid(char[][] grid) {
-		for (int y = 0; y < grid.length; y++) {
+	/**
+	 * Pretty print the contents of a grid to standard out.
+	 * 
+	 * @param grid
+	 *        The grid to be printed.
+	 */
+	public static void printGrid(final char[][] grid) {
+		for (final char[] element : grid) {
 			for (int x = 0; x < grid[0].length; x++) {
-				System.out.print(grid[y][x] + " ");
+				System.out.print(element[x] + " ");
 			}
+
 			System.out.println("");
 		}
-	}
-
-	private static char[][] createGrid() {
-		char[][] grid = new char[4][4];
-		// y, x
-		grid[0][0] = 'p';
-		grid[0][1] = 'e';
-		grid[0][2] = 'o';
-		grid[0][3] = 'r';
-		grid[1][0] = 'o';
-		grid[1][1] = 'g';
-		grid[1][2] = 'b';
-		grid[1][3] = 't';
-		grid[2][0] = 't';
-		grid[2][1] = 'l';
-		grid[2][2] = 'e';
-		grid[2][3] = 'o';
-		grid[3][0] = 'u';
-		grid[3][1] = 'e';
-		grid[3][2] = 'v';
-		grid[3][3] = 'n';
-		return grid;
 	}
 }
